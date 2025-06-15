65c7e7f455f50f6d72c17dd68d12521f94008a70
Bjarne <bjarne_priebe@hotmail.com>
2025-06-15 09:28:21 +0200

    Updates

---
+++ precache-manifest.29bd945fcffe72dd7b3ce2e6870c8f3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb8462c71611d6c77ba2b5538188d18",
+    "url": "/priebek.github.io/index.html"
+  },
+  {
+    "revision": "b72c6a02ab067e49bddd",
+    "url": "/priebek.github.io/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "a1084ef6ef278385f3e5",
+    "url": "/priebek.github.io/static/css/main.36918b24.chunk.css"
+  },
+  {
+    "revision": "b72c6a02ab067e49bddd",
+    "url": "/priebek.github.io/static/js/2.4c102bb7.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/priebek.github.io/static/js/2.4c102bb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1084ef6ef278385f3e5",
+    "url": "/priebek.github.io/static/js/main.3e3c3350.chunk.js"
+  },
+  {
+    "revision": "9df10c1c79b90bdfa252",
+    "url": "/priebek.github.io/static/js/runtime-main.90a7cfd9.js"
+  }
+]);
